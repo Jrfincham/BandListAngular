3c4c25ccf222b01a3cc66df05e4c91cf1558275f
Jason Robert Fincham <JRFincham@yahoo.com>
2015-03-02 03:48:31 -0500

    deployed

--- app/scripts/user/user.factory.js
+++ app/scripts/user/user.factory.js
@@ -1,10 +1,10 @@
 ;(function() {
 
   angular.module('BandsAtl')
-  .factory('UserFactory', ['$scope', 'PARSE', '$location', '$rootScope',
-    function($scope, PARSE, $location, $rootScope){
-      
-    }
+  .factory('UserFactory', [
+
+
+
 
 
   ]);
